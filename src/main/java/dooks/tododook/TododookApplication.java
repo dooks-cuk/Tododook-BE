df74ac6860a9c172fe342d45246553feed65fb4e
sor999 <sorsor999@naver.com>
2024-06-08 20:05:59 +0900

    feat: @EnableJpaAuditing 추가

--- src/main/java/dooks/tododook/TododookApplication.java
+++ src/main/java/dooks/tododook/TododookApplication.java
@@ -2,8 +2,10 @@ package dooks.tododook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TododookApplication {
 
 	public static void main(String[] args) {
